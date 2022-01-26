98d7bde4bd3f290a6142ce40d83018908d12c89d
Ernesto Cediel <ecedielcc@gmail.com>
2022-01-25 20:14:27 -0800

    Added first iteration of entropy calculation

--- calc_entropy.py
+++ calc_entropy.py
@@ -1,2 +1,9 @@
+import numpy as np
+
+def entropy(probs):
+    """Assumes that it is a numpy array and that it is a valid probability vector."""
+    entropy=-(probs*np.log(probs))
+    return entropy.value
+    
 if __name__ == "__main__":
-        pass
+    pass
